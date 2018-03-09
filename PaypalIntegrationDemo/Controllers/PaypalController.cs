fb97dac8bc81727077b3bcf3f3924564d8c591a3
yeasinmahi <yeasinmahi72@yahoo.com>
2018-03-09 15:30:09 +0600

    initial commit

---
+++ PaypalIntegrationDemo/Controllers/PaypalController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PaypalIntegrationDemo.Controllers.Paypal;
+
+namespace PaypalIntegrationDemo.Controllers
+{
+    public class PaypalController : Controller
+    {
+        // GET: Paypal
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Success()
+        {
+            ViewBag.result = PDTHolder.Success(Request.QueryString.Get("tx"));
+            return View("Success");
+        }
+    }
+}
